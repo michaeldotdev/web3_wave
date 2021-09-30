69442010ff909926383fad7c762551318efde147
Michael Su <michaelsu.jobs@gmail.com>
2021-09-30 11:01:44 -0700

    Implement run.js to run basic contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo! This is the first smart contract!");
+    }
+}
