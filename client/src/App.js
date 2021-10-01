009ec4932b046dcc333eaa34f585099e5024a174
Michael Su <michaelsu.jobs@gmail.com>
2021-09-30 19:30:33 -0700

    Implement wallet checking and small error fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  const checkWallet = () => {
+    const { ethereum } = window;
+
+    ethereum
+      ? console.log("Make sure your wallet is connected!")
+      : console.log("Wallet is connected!", ethereum);
+  }
+
   const wave = () => {
 
   }
+
+  useEffect(() => {
+    checkWallet()
+  }, [])
+
   return (
     <div className="mainContainer">
       <div className="dataContainer">
         <div className="header">
-         <h1>🤜🏼 Hey There!</h1>
+          <h1>🤜🏼 Hey There!</h1>
         </div>
 
         <div className="bio">
